0c6c0b1ffef04c8f9cb82cd2ec5df94c52ba31d0
Jéssica Amorim <jessicamorim.42@gmail.com>
2016-12-30 19:50:47 -0200

    created courses app

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.db.models import Q
+
+class CourseManager(models.Manager):
+
+    def search(self, query):
+        return self.get_queryset().filter(Q(name__icontains=query) | Q(description__icontains=query))
 
 class Course(models.Model):
 
@@ -8,6 +14,18 @@ class Course(models.Model):
     start_date = models.DateField(
         'Data de Início', null = True, blank=True
     )
-    image = models.ImageField(upload_to='courses/images', verbose_name='Imagem', null=True, blank=True)
+    image = models.ImageField(
+        upload_to='courses/images', verbose_name='Imagem',
+        null=True, blank=True
+    )
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
     updated_at= models.DateTimeField('Atualizado em', auto_now=True)
+    objects = CourseManager()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+        ordering = ['name']
