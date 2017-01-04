041ea8918b7e5602842cf073132181aa7d643eb8
Jéssica Amorim <jessicamorim.42@gmail.com>
2017-01-03 22:51:50 -0200

    Creating a form

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^', include('core.urls', namespace='core')),
-    url(r'^cursos/', include('courses.urls', namespace='courses')),
+    url(r'^', include('simplemooc.core.urls', namespace='core')),
+    url(r'^cursos/', include('simplemooc.courses.urls', namespace='courses')),
     url(r'^admin/', include(admin.site.urls)),
 ]
 
