cb961afc1bc7e18af2d84c3efce5f135928fa435
Jéssica Amorim <jessicamorim.42@gmail.com>
2017-01-17 20:05:40 -0200

    Custom register form

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib.auth.views import login
-
+from . import views
 urlpatterns = [
     url(r'^entrar/$', login,
         {'template_name':'accounts/login.html'}, name='login'),
+    url(r'^cadastre-se/$', views.register, name='register'),
 
 ]
